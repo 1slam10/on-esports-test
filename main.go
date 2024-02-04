9b680cfb7899c8eabcbfca29f8ac6e1612ee9d1f
Islam <adilkhanislam9@icloud.com>
2024-02-05 05:43:30 +0600

    Add: Added TODO for future improvements

--- main.go
+++ main.go
@@ -21,3 +21,19 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+// The initial project should have been more complex and more creative than what I have now. 
+// The features that I wanted to add initially listed below
+
+// TODO: Add command to check given player’s avg. statistics
+// TODO: Add command to check given player's performance on given match
+// TODO: Add command to check past game result of given team in the given discipline
+// TODO: Add command to get upcoming games and tournaments of specified team
+// TODO: Add command to get H2H (Head-to-Head) stats of two commands
+// TODO: Add "gamification": to implement coins
+// TODO: Add command to create a bet on specified match using those coins
+// TODO: Add command to get user's rating based on their balance / bet winning rate etc.
+
+// Was very busy by stressing about that I don't understand anything, but in fact, it was much easier than expected.
+// (I don't mean it is easy, code quality is not the best tho, could be better, but it is what it is, try to make it sexy).
+// Anyways, there are a lot if things to do that I could do most earlier
